2edd8f3db6e1c52fa5b67cafea1b5af52bb2c6cd
Thomas Rowlands <thomas.rowlands@faire.com>
2021-03-27 17:05:39 -0400

    Update to temporary landing page

--- js/script.js
+++
@@ -1,9 +0,0 @@
-const showInformation = () => {
-  document.getElementsByClassName('landing')[0].style.display = 'none';
-  document.getElementsByClassName('information')[0].style.display = 'flex';
-}
-
-const showLanding = () => {
-  document.getElementsByClassName('information')[0].style.display = 'none';
-  document.getElementsByClassName('landing')[0].style.display = 'flex';
-}
